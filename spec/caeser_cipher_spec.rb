56b81d230917adf0a0ebff1cacb9f25722eea689
kckuei <kevinkuei@gmail.com>
2022-09-22 17:43:49 -0700

    Add first rspec test

--- spec/caeser_cipher_spec.rb
+++ spec/caeser_cipher_spec.rb
@@ -1,4 +1,9 @@
 # spec/caeser_cipher_spec.rb
-require '.lib/ceaser_cipher' # this is relative to the .rspec file
+require './lib/caeser_cipher' # this is relative to the .rspec file
 
-puts caesar_cipher('What a string!', 5)     # "Bmfy f xywnsl!"
+describe CaeserCipher do
+  it "Shifts 'What a string' by 5 places to 'Bmfy f xywnsl!'" do
+    caeser_cipher = CaeserCipher.new
+    expect(caeser_cipher.cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+  end
+end
